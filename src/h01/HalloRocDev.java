d633ce1c5f3e1c61ec263ba7f43916e97187dffd
Dawid K <dkujawskiii@gmail.com>
2018-08-29 11:26:31 +0200

    Hoi.

--- src/h01/HalloRocDev.java
+++ src/h01/HalloRocDev.java
@@ -6,6 +6,6 @@ package h01;
 public class HalloRocDev {
 
     public static void main(String[] args) {
-        System.out.println("Hallo klas 7A!");
+        System.out.println("Ich sprachen nit zo gut nederlandsh!");
     }
 }
