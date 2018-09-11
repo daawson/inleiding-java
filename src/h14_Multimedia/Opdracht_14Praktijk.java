d5965b725917213c73b92a7ae25597a7ca6ea475
Dawid K <dkujawskiii@gmail.com>
2018-09-11 09:55:47 +0200

    Minor update.

--- src/h14_Multimedia/Opdracht_14Praktijk.java
+++ src/h14_Multimedia/Opdracht_14Praktijk.java
@@ -18,7 +18,7 @@ public class Opdracht_14Praktijk extends Applet {
     private ArrayList<Integer> focus = new ArrayList<>();
 
     public void init(){
-        message = new Label("Write 1, 2 or to play!");
+        message = new Label("Write 1, 2 or 3 to play!");
         input = new TextField();
 
         bOK = new Button("Play!");
@@ -28,7 +28,7 @@ public class Opdracht_14Praktijk extends Applet {
             else if(CurrentState == State.Reset){
                 ReinitArray();
                 rockCount = 23;
-                message.setText("Write 1, 2 or to play!");
+                message.setText("Write 1, 2 or 3 to play!");
                 bOK.setLabel("Play");
                 CurrentState = State.Play;
                 repaint();
@@ -37,9 +37,11 @@ public class Opdracht_14Praktijk extends Applet {
 
         ReinitArray();
 
-        add(message);
+
         add(input);
         add(bOK);
+        message.setPreferredSize(new Dimension(200,25));
+        add(message);
     }
 
     private void PlayWith(int a){
@@ -50,6 +52,7 @@ public class Opdracht_14Praktijk extends Applet {
 
             if(rockCount == 0){
                 message.setText("You lost! :(");
+                bOK.setLabel("Reset");
                 CurrentState = State.Reset;
             }
             else {
@@ -62,27 +65,8 @@ public class Opdracht_14Praktijk extends Applet {
 
     }
 
-    private void DoMagic(){
-        //region TEST
-        /*
-        int closest = Helper.getClosest(focus, rockCount);
-        int diff = rockCount-closest;
-        System.out.println(closest + " " + diff);
-
-        if(diff != 0) {
-            if (diff < 4 && diff > 0) {
-                rockCount -= diff;
-
-            } else if (diff < 0) {
-                rockCount = rockCount - (-diff);
-            }
-        }
-        else{
-                rockCount -= 3;
-        }
-        */
-        //endregion
-
+    private void DoMagic(){ // 1,5,9,13,17,21
+        int lastnum = rockCount;
         for (int i = 0; i < focus.size(); i++) {
             int checker = focus.get(i);
             System.out.println(checker);
@@ -101,8 +85,12 @@ public class Opdracht_14Praktijk extends Applet {
                 System.out.println(checker + " " + (rockCount-3));
                 focus.remove(focus.get(i));
                 break;
-            } else {
-                rockCount-=3;
+            } else if(checker == rockCount) {
+                double r = Math.random();
+                int c = (int)(r*3+1);
+                System.out.println(checker + " " + (rockCount-c));
+                rockCount-=c;
+                focus.remove(focus.get(i));
                 break;
             }
         }
@@ -112,20 +100,21 @@ public class Opdracht_14Praktijk extends Applet {
             CurrentState = State.Reset;
             bOK.setLabel("Reset");
         }else{
-            message.setText("Current rocks: " + rockCount);
+            message.setText("Current rocks: " + rockCount + " BOT took: " + (lastnum-rockCount) + " rocks!");
         }
     }
 
     private void ReinitArray(){
+        focus.clear();
         focus.add(21);focus.add(17);focus.add(13);focus.add(9); focus.add(5); focus.add(1);
     }
 
     public void paint(Graphics g){
         int row = 0, col = 0;
         for(int x = 0; x < rockCount; x++){
-            if(x % 4 == 0){col++; row = 0;}
+            if(x % 8 == 0){col++; row = 0;}
             g.setColor(Color.gray);
-            g.fillOval(25+row*25, 25+col*25, 20,20);
+            g.fillOval(25+row*35, 25+col*35, 20,20);
             row++;
         }
 
